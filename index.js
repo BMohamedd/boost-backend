b67f9569171d308c2cb9249f433367a811d52411
Belhadj Mohamed <rafikgamer8@gmail.com>
2022-07-02 14:56:51 +0200

    test commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const gen_report = require("./lighthouse_service");
 
 const app = express();
 const port = 8000 || process.env.PORT;
@@ -15,19 +14,6 @@ app.use(
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.get("/report", (req, res) => {
-  const url = req.query.url;
-  const device = req.query.device;
-  console.log(req.query);
-  gen_report(url, device)
-    .then((report) => {
-      res.send(report);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
 });
 
 app.listen(port, () => {
